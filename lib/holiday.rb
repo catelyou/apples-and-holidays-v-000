88a82270027f21eaf1ac313b71e48e3c8fa89923
Jaclyn Hall <jaclynchall@gmail.com>
2018-07-24 18:00:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,11 +63,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holidays|
-    puts "#{season.capitalize!}:"
+    puts "#{season.capitalize}:"
      holidays.each do |holiday, item|
-       puts "#{holiday.to_s.split("_").collect {|i| i.capitalize!}.join(" ")}: #{item.join(", ")}"
+       puts "#{holiday.to_s.split("_").collect {|i| i.capitalize}.join(" ")}: #{item.join(", ")}"
      end
-  end 
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
